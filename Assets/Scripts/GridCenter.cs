10bf8886f6453471ebecfc864df8ca2686fcc095
koake <pq2dnv@qq.com>
2017-08-13 19:43:13 +0800

    做一个3xiao游戏
    第一次提交。
    优化了从中间自己生成，虽然并没有很厉害的算法

---
+++ Assets/Scripts/GridCenter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridCenter : MonoBehaviour
+{
+    public int gridSize = 50;
+    public int gridInterval = 10;
+    public int colCount;
+    public int rowCount;
+    public GameObject gridModel;
+    void Start()
+    {
+        for (int i = 0; i < colCount; i++)
+        {
+            for (int j = 0; j < rowCount; j++)
+            {
+                GameObject grid = Instantiate(gridModel) as GameObject;
+                grid.transform.SetParent(this.transform);
+                var x = (j - rowCount / 2) * (this.gridSize + this.gridInterval);
+                var y = (i - rowCount / 2) * (this.gridSize + this.gridInterval);
+                grid.transform.localPosition = new Vector3(x, y, 0);
+            }
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
